3d2186356979b44c610f6ff2e4cf2cce99540a53
Towhid Savari <towhid.savari@gmail.com>
2020-06-03 16:09:32 +0430

    Create django project and app

---
+++ rest_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\tohid savari\desktop\django projects\profiles_rest_api\rest_venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
